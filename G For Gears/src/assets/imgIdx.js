c0f4a934ec75e85329b02e80db586f038aace67a
karmakarswaraj <swarajkarmakar5@gmail.com>
2024-03-22 16:13:55 +0530

    Final commit

--- G For Gears/src/assets/imgIdx.js
+++ G For Gears/src/assets/imgIdx.js
@@ -1,9 +1,9 @@
-import BagImg1 from "./bag1.png"; 
+import BagImg1 from "./bag1.png";
 import BagImg2 from "./bag2.png";
 import BagImg3 from "./bag3.png";
 import BagImg4 from "./bag4.png";
 import BagImg5 from "./bag5.png";
-import BagImg6 from "./bag6.png"; 
+import BagImg6 from "./bag6.png";
 import BagImg7 from "./bag7.png";
 import gloveImg1 from "./gloves1.png";
 import gloveImg2 from "./gloves2.png";
@@ -28,11 +28,11 @@ import shoeImg6 from "./shoe6.png";
 import shoeImg7 from "./shoe7.png";
 
 import Logo from "./Logo.png";
-import bigImg from "./f5.png"
-import img1 from './f1.png'
-import img2 from './f2.png'
-import img4 from './f4.png'
-import img5 from './f5.png'
+import bigImg from "./f5.png";
+import img1 from "./f1.png";
+import img2 from "./f2.png";
+import img4 from "./f4.png";
+import img5 from "./f5.png";
 
 import glovesImg from "./gloves.png";
 import shoesImg from "./shoes.png";
@@ -42,11 +42,46 @@ import backpackImg from "./backpack.png";
 import FbagImg from "./backpack2.png";
 import FshoeImg from "./shoes2.png";
 import FshoeImg2 from "./shoes3.png";
-export{
-    BagImg1, BagImg2, BagImg3, BagImg4, BagImg5, BagImg6, BagImg7,
-    gloveImg1, gloveImg2, gloveImg3, gloveImg4, gloveImg5, gloveImg6, gloveImg7,
-    helmetImg1, helmetImg2, helmetImg3, helmetImg4, helmetImg5, helmetImg6, helmetImg7,
-    shoeImg1, shoeImg2, shoeImg3, shoeImg4, shoeImg5, shoeImg6, shoeImg7,
-    Logo,bigImg,img1,img2,img4,img5,glovesImg,shoesImg,helmetImg,backpackImg,
-    FbagImg,FshoeImg,FshoeImg2,
-}
+export {
+  BagImg1,
+  BagImg2,
+  BagImg3,
+  BagImg4,
+  BagImg5,
+  BagImg6,
+  BagImg7,
+  gloveImg1,
+  gloveImg2,
+  gloveImg3,
+  gloveImg4,
+  gloveImg5,
+  gloveImg6,
+  gloveImg7,
+  helmetImg1,
+  helmetImg2,
+  helmetImg3,
+  helmetImg4,
+  helmetImg5,
+  helmetImg6,
+  helmetImg7,
+  shoeImg1,
+  shoeImg2,
+  shoeImg3,
+  shoeImg4,
+  shoeImg5,
+  shoeImg6,
+  shoeImg7,
+  Logo,
+  bigImg,
+  img1,
+  img2,
+  img4,
+  img5,
+  glovesImg,
+  shoesImg,
+  helmetImg,
+  backpackImg,
+  FbagImg,
+  FshoeImg,
+  FshoeImg2,
+};
